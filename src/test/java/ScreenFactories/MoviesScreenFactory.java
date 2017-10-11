18512be275e929e299b2f130cc5d6e152e191d9a
arsenal <strigunovqa@gmail.com>
2017-10-10 18:41:58 -0700

    added elements to MoviesScreenFactory

--- src/test/java/ScreenFactories/MoviesScreenFactory.java
+++ src/test/java/ScreenFactories/MoviesScreenFactory.java
@@ -12,5 +12,16 @@ public class MoviesScreenFactory {
     @AndroidFindBy(id = "btnHamburger")
     public MobileElement profileButton;
 
+    @AndroidFindBy (id = "tvTitle")
+    public MobileElement movieTitle;
+
+    @AndroidFindBy (id = "iv_movie_date_active_select")
+    public MobileElement movieDateActiveCheckMark;
+
+    @AndroidFindBy (id = "rlToggleButton")
+    public MobileElement interestedButton;
+
+
+
 
 }
